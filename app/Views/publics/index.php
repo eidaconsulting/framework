3ab31a0f2df9cb1cc4764fa657020e1ac4e345c5
EIDA CONSULTING <contact@eidaconsulting.com>
2020-03-26 19:33:32 +0100

    Ajust alert box in mobile

--- app/Views/publics/index.php
+++ app/Views/publics/index.php
@@ -3,9 +3,6 @@
         <div class="py-5 row">
             <div class="col text-center">
                 <h1>Bienvenue sur la page d'accueil</h1>
-                <div id="alert" class="is-alerte-message alert alert-success">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ducimus ea hic ipsum minima quidem ratione rem, vel vero! Animi enim est eveniet expedita fugiat obcaecati quas quis quos. Dicta.
-                </div>
 
             </div>
         </div>
